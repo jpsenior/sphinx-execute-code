344cc15df059c1a8afc49c3f1d3539f1ae5ab116
JP Senior <jp.senior@gmail.com>
2017-08-01 00:31:56 -0600

    Readme as a function

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
+def readme():
+    with open('Readme.rst', 'r') as f:
+        return f.read()
 setup(
         name='sphinx_execute_code',
-        version='0.2a1',
+        version='0.2a2',
         platforms=['any'],
         packages=['sphinx_execute_code'],
         url='https://github.com/jpsenior/sphinx-execute-code',
@@ -10,6 +13,7 @@ setup(
         author='JP Senior',
         author_email='jp.senior@gmail.com',
         description='Sphinx support for execution of python code in sphinx documentation from code blocks or files',
+        long_description=readme(),
         install_requires=['docutils', 'sphinx'],
         classifiers=[
             'Development Status :: 3 - Alpha',
